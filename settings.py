28c1a455679e774d0a29cab097dddb5c367cd1f5
tjsepp <tjsepp@gmail.com>
2020-03-12 13:55:25 -0600

    Updated the settings page to include local DB connections.

--- settings.py
+++ settings.py
@@ -1,6 +1,17 @@
+'''
+Snowflake connection information
+'''
 SNOWFLAKE_ACCOUNT =''
 SNOWFLAKE_USER =''
 SNOWFLAKE_PW=''
 SNOWFLAKE_DATABASE= ''
 SNOWFLAKE_WAREHOUSE=''
 SNOWFLAKE_SCHEMA=''
+
+'''
+SQL server connections information
+'''
+LOCAL_SERVER=''
+LOCAL_DB=''
+LOCAL_UN=''
+LOCAL_PW=''
